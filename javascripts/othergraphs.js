7405fa99e1fcad644d7d61f5efa543b97c1bc42e
K5rovski <glupoime101@gmail.com>
2015-01-23 21:42:38 +0100

    Added Doughnut

---
+++ javascripts/othergraphs.js
@@ -0,0 +1,45 @@
+makeDoughnut=function(Data){
+	 var start = new Date().getTime(); //Time Start
+
+	for (var i=0;i<Data.length;i++){
+		Data[i]["indexLabel"]=Data[i]["printmonth"];
+		Data[i]["y"]=Data[i]["averageval"];
+		}
+
+var middle=new Date().getTime();
+         
+		 var chart = new CanvasJS.Chart("chartContainer2", 
+		  {
+      title:{
+        text: "Skopje",
+        fontFamily: "Impact",
+        fontWeight: "normal"
+      },
+
+      legend:{
+        verticalAlign: "bottom",
+        horizontalAlign: "center"
+      },
+      data: [
+      {
+        //startAngle: 45,
+       indexLabelFontSize: 20,
+       indexLabelFontFamily: "Garamond",
+       indexLabelFontColor: "darkgrey",
+       indexLabelLineColor: "darkgrey",
+       indexLabelPlacement: "outside",
+       type: "doughnut",
+    //   showInLegend: true,
+       dataPoints: Data
+     }
+     ]
+   });
+
+    chart.render();
+	
+		// Time end
+		var end = new Date().getTime();
+		
+		$('#TimeSpent2').text('Time loading data='+(middle-start)+
+		' mills. Time showing graph='+(end-middle)+' mils.');
+}
