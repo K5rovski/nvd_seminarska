8630b7b58dd69169ff4d994ea019604cf94c346e
K5rovski <glupoime101@gmail.com>
2015-01-24 14:52:28 +0100

    Added Bubble

--- javascripts/othergraphs.js
+++ javascripts/othergraphs.js
@@ -146,3 +146,62 @@ var middle=new Date().getTime();
 		$(timediv).text('Time loading data='+(middle-start)+
 		' mills. Time showing graph='+(end-middle)+' mils.');
 }
+
+makeBubble=function(Data,chartdiv,timediv){
+	 var start = new Date().getTime(); //Time Start
+	var newD=[];
+	for (var i=0;i<Data.length;i++){
+		newD.push({})
+		newD[i]["city"]=Data[i]["city"];
+		newD[i]["z"]=+(Data[i][Mpref]).toFixed(2);
+		newD[i]["y"]=Data[i]["GDP"];
+		newD[i]["x"]=Data[i]["Pop_Density"];
+	//	var dstr=Data[i][Mpref+"Day"].split('_');
+	//	var ddate=new Date(dstr);
+	//	newD[i]["printday"]=ddate.toDateString();
+	//	Data[i]["x"]=(i+1)*10;
+		}
+
+var middle=new Date().getTime();
+      //   console.log(Data);
+		 var chart = new CanvasJS.Chart(chartdiv, 
+		  {
+      title:{
+        text: "Comparing Pollution with Population and GDP",
+        fontFamily: "Impact",
+        fontWeight: "normal"
+      },
+		legend:{
+        verticalAlign: "bottom",
+        horizontalAlign: "left"
+
+      },
+	   axisY:{
+		title:'GDP/capita of home country'
+	} ,
+     axisX:{
+       title: "Population Density",
+  //     gridThickness: 1,
+    //   tickLength: 10
+      },
+      data: [
+      {
+      legendText: "Size of Bubble Represents Average Daily PM10 Pollution",
+      toolTipContent: "<strong>{city}</strong> <hr/>"+
+	  "GDP: {y}<br/> Population Density: {x} /km2<br/> Avg. Daily PM10: {z} ug/m3",
+	  type: "bubble",
+       showInLegend: true,
+       legendMarkerType:"circle",
+	   dataPoints: newD      
+     }
+     ]
+   });
+
+    chart.render();
+	
+		// Time end
+		var end = new Date().getTime();
+		
+		$(timediv).text('Time loading data='+(middle-start)+
+		' mills. Time showing graph='+(end-middle)+' mils.');
+}
